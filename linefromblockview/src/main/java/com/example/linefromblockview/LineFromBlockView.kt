9687f68c3f5cad904f4cd1fb2a39060fde40b46d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-13 01:29:26 +0530

    adding Canvas extension methods

--- linefromblockview/src/main/java/com/example/linefromblockview/LineFromBlockView.kt
+++ linefromblockview/src/main/java/com/example/linefromblockview/LineFromBlockView.kt
@@ -29,3 +29,29 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+
+fun Canvas.drawLineFromBlock(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sc2)
+    save()
+    translate(0f, -h / 2 + (h / 2) * sc1 + (w / 2 + size) * sc3)
+    drawRect(RectF(-size, -size, 0f, 0f), paint)
+    drawLine(0f, 0f, 0f, size * sc1, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLFBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineFromBlock(scale, w, h, paint)
+}
