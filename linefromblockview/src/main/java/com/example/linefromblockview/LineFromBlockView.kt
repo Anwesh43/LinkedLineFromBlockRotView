12189a64f61966c821028a389340f4b6240f85c8
Anwesh43 <anweshthecool0@gmail.com>
2021-06-13 01:17:41 +0530

    adding constants and libraries

--- linefromblockview/src/main/java/com/example/linefromblockview/LineFromBlockView.kt
+++ linefromblockview/src/main/java/com/example/linefromblockview/LineFromBlockView.kt
@@ -1,2 +1,28 @@
 package com.example.linefromblockview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#01579B",
+    "#00C853",
+    "#FFC107",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
+
