639b0a110304e19c0cdfeec37fa8c0cf77352549
Anwesh43 <anweshthecool0@gmail.com>
2021-06-13 01:41:20 +0530

    adding static create method

--- linefromblockview/src/main/java/com/example/linefromblockview/LineFromBlockView.kt
+++ linefromblockview/src/main/java/com/example/linefromblockview/LineFromBlockView.kt
@@ -211,4 +211,13 @@ class LineFromBlockView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineFromBlockView {
+            val view : LineFromBlockView = LineFromBlockView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
